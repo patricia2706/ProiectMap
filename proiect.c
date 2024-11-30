3f59b0a685f0c0acb805fa57d6b35e7953639ad9
patricia2706 <talpa.patricia.clarisa@gmail.com>
2024-11-30 23:14:51 +0200

    Creare functie + formatare document
    Am adaugat functia insert care cauta pozitia in care trebuie sa fie pus un nod cu cheia data si il insereaza in acel loc.

--- proiect.c
+++ proiect.c
@@ -1,21 +1,50 @@
-#include<stdio.h>
+#include <stdio.h>
 #include <stdlib.h>
 
-typedef struct Nod{
+typedef struct Nod
+{
     int cheie;
     struct Nod *st;
     struct Nod *dr;
-}Nod;
+} Nod;
 
-Nod *init(int cheia){
-    Nod *nod=(Nod*)malloc(sizeof(Nod));
-    nod->cheie=cheia;
-    nod->st=NULL;
-    nod->dr=NULL;
+Nod *init(int cheia)
+{
+    Nod *nod = (Nod *)malloc(sizeof(Nod));
+    nod->cheie = cheia;
+    nod->st = NULL;
+    nod->dr = NULL;
     return nod;
 }
 
-int main(){
+void insert(Nod *crtCopil, Nod *parinte, int cheia)
+{
+    if (crtCopil == NULL) //inserarea nodului
+    {
+        Nod *nod = init(cheia); //se creeaza un nod nou folosind cheia data 
+        if (cheia >= parinte->cheie)
+        {
+            parinte->dr = nod;
+        }
+        else
+        {
+            parinte->st = nod;
+        }
+        return;
+    }
+
+    if (cheia >= crtCopil->cheie) //controleaza directia recursiunii (parcurgerea arborelui)
+    {
+        insert(crtCopil->dr, crtCopil, cheia);
+    }
+    else
+    {
+        insert(crtCopil->st, crtCopil, cheia);
+    }
+}
+
+int main()
+{
 
     return 0;
 }
